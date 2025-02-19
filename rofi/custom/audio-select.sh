00848da8479cdbbdf8ab09a94fd9f94e8e04d38b
dablenparty <anoptimisticcouch@gmail.com>
2025-02-19 17:01:54 -0500

    refactor(rofi/audio-select.sh): shows pretty names instead of sinks

--- rofi/custom/audio-select.sh
+++ rofi/custom/audio-select.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 # taken from: https://adamsimpson.net/writing/getting-started-with-rofi
 
-source="$(pactl list short sinks | cut -f 2 | rofi -i -dmenu -p "Change audio:")"
+declare -A sources
+i=0
+while read -r line; do
+  # if the line is empty, end this section and continue
+  if [[ "$line" =~ ^\s*$ ]]; then
+    i=$((i + 1))
+    continue
+  fi
+  sources[$i]="${sources[$i]}\n$line"
+done < <(pactl list sinks)
 
-if [[ -z "$source" ]]; then
+declare -A source_names
+for i in $(seq 0 "$i"); do
+  # TODO: if sink starts with 'raop_sink', label it as (AirPlay)
+  source_names[$i]="$(echo -e "${sources[$i]}" | rg 'Description: ' | awk -F ': ' '{print $2}')"
+  i=$((i + 1))
+done
+
+selected_idx="$(for i in $(seq 0 ${#source_names[@]}); do echo "${source_names[$i]}"; done | rofi -dmenu -format "i" -i -p "Change audio:")"
+
+if [[ -z "$selected_idx" ]]; then
   exit 0
 fi
+
+source="${sources[$selected_idx]}"
+source_name="${source_names[$selected_idx]}"
+sink="$(echo -e "$source" | rg 'Name: ' | awk -F ': ' '{print $2}')"
+
+printf "Selected Audio Device Index: %d\nName: %s\nSink: %s\n" "$selected_idx" "$source_name" "$sink"
 
 inputs="$(pactl list sink-inputs short | cut -f 1)"
 
 for input in $inputs; do
-  pactl move-sink-input "$input" "$source"
+  pactl move-sink-input "$input" "$sink"
 done
 
-pactl set-default-sink "$source"
-# TODO: get device.description for this instead
-notify-send "Successfully set audio device" "$source"
+pactl set-default-sink "$sink"
+notify-send "Successfully set audio device" "$source_name"
