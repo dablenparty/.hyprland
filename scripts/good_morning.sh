648f5900a7d684c6533fad2930f08ac725ed030a
dablenparty <anoptimisticcouch@gmail.com>
2025-04-01 17:30:24 -0400

    refactor(good_morning.sh)!: update system after rustup
    ONE TIME it happened where the system update needed Rust to update
    first, but it hadn't, so it downloaded rust from the repos to get the
    latest version. I didn't notice and it caused me some grief, so now
    rustup updates first.

--- scripts/good_morning.sh
+++ scripts/good_morning.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
+# e: fail script if any command fails
+# x: print every command before it's run
+set -ex
 
-paru
-rustup self update
+# TODO: args with getopts or GNU getopt
+# -n: pass --noconfirm to paru
+# -r: update rustup stable
+
+# NOTE: rustup self-updates on any invocation of the "update" subcommand
+# If that ever changes, uncomment the line below
+# rustup self update
 rustup toolchain update nightly
+
+# I don't like using sudoloop, but this script is an exception
+paru --sudoloop
