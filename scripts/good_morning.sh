35163482bb2fd1ce1f714cb364f18c792718fe30
dablenparty <anoptimisticcouch@gmail.com>
2025-04-02 12:20:02 -0400

    feat(good_morning.sh): add command line flags

--- scripts/good_morning.sh
+++ scripts/good_morning.sh
@@ -1,17 +1,47 @@
 #!/usr/bin/env bash
 
 # e: fail script if any command fails
-# x: print every command before it's run
-set -ex
+set -e
 
-# TODO: args with getopts or GNU getopt
-# -n: pass --noconfirm to paru
-# -r: update rustup stable
+if [ -z "$OPTIND" ]; then
+  OPTIND=1
+fi
+
+option_noconfirm=false
+option_update_rustup=false
+
+while getopts "hnr" opt; do
+  case $opt in
+  h)
+    printf "usage: %s [-hnr]\n  -h    show this help text\n  -n    pass --noconfirm to paru\n  -r    update rustup stable+nightly\n" "$0" 1>&2
+    exit 1
+    ;;
+  n)
+    option_noconfirm=true
+    ;;
+
+  r)
+    option_update_rustup=true
+    ;;
+
+  ?)
+    printf "Invalid option: -%s\n" "$OPTARG" 1>&2
+    exit 1
+    ;;
+  esac
+done
 
 # NOTE: rustup self-updates on any invocation of the "update" subcommand
 # If that ever changes, uncomment the line below
 # rustup self update
-rustup toolchain update nightly
+if [[ $option_update_rustup = true ]]; then
+  rustup update
+else
+  rustup toolchain update nightly
+fi
 
-# I don't like using sudoloop, but this script is an exception
-paru --sudoloop
+if [[ $option_noconfirm = true ]]; then
+  paru -Syu --noconfirm --sudoloop
+else
+  paru -Syu --sudoloop
+fi
