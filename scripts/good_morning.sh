979582ca7bc5647e6c5408074dfec75f9d26243f
dablenparty <anoptimisticcouch@gmail.com>
2025-04-18 02:05:47 -0400

    feat(good_morning): add -c flag to update couchlab

--- scripts/good_morning.sh
+++ scripts/good_morning.sh
@@ -7,13 +7,17 @@ if [ -z "$OPTIND" ]; then
   OPTIND=1
 fi
 
+option_update_couchlab=false
 option_noconfirm=false
 option_update_rustup=false
 
-while getopts "hnr" opt; do
+while getopts "chnr" opt; do
   case $opt in
+  c)
+    option_update_couchlab=true
+    ;;
   h)
-    printf "usage: %s [-hnr]\n  -h    show this help text\n  -n    pass --noconfirm to paru\n  -r    update rustup stable+nightly\n" "$0" 1>&2
+    printf "usage: %s [-chnr]\n  -c    update couchlab too\n  -h    show this help text\n  -n    pass --noconfirm to paru\n  -r    update rustup stable+nightly\n" "$0" 1>&2
     exit 1
     ;;
   n)
@@ -40,8 +44,22 @@ else
   rustup toolchain update nightly
 fi
 
+paru_cmd=""
 if [[ $option_noconfirm = true ]]; then
-  paru -Syu --noconfirm --sudoloop
+  paru_cmd="paru -Syu --noconfirm --sudoloop"
 else
-  paru -Syu --sudoloop
+  paru_cmd="paru -Syu --sudoloop"
 fi
+
+# NOTE: paru has an issue that causes it to exit with code 2 when using a custom PKGBUILD.
+# To get around it, I'm just allowing it to error out and still continue the script.
+# When it's fixed, I should remove this workaround.
+# see: https://github.com/Morganamilo/paru/issues/1234
+set +e
+eval "$paru_cmd"
+set -e
+if [[ $option_update_couchlab = true ]]; then
+  ssh -tt couchlab "$paru_cmd; systemctl reboot"
+fi
+
+echo "Don't forget to reboot!"
