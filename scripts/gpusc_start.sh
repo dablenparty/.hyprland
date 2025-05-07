d6a623c8ee4fe3f97282f1ce20c1ca30d3bdeb84
dablenparty <anoptimisticcouch@gmail.com>
2025-05-06 20:47:21 -0400

    feat(gpusc_start.sh)!: add confirmation before recording

--- scripts/gpusc_start.sh
+++ scripts/gpusc_start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -xeo pipefail
+set -eo pipefail
 
 # TODO: cli args with getopts
 # for passing args to gpu-screen-recorder, pass all args that come after '--'
@@ -31,15 +31,30 @@ for dev in "${selected_audio_devices[@]}"; do
   GPUSC_ARGS+=(-a "$dev")
 done
 
-GPUSC_ARGS=("${GPUSC_ARGS[@]}" "$@")
-
-systemd-inhibit --what "sleep:idle:shutdown" --who "gpu-screen-recorder" --why "recording screen" \
-  gpu-screen-recorder \
-  -f 60 \
-  -fm cfr \
-  -k hevc \
-  -bm qp \
-  -q very_high \
-  -tune performance \
-  -ac aac \
+# construct command from dynamic args
+GPUSC_ARGS=(
+  gpu-screen-recorder
+  -f 60
+  -fm cfr
+  -k hevc
+  -bm qp
+  -q very_high
+  -tune performance
+  -ac aac
   "${GPUSC_ARGS[@]}"
+  "$@"
+)
+
+echo "GPUSC command: ${GPUSC_ARGS[*]}"
+
+# TODO : extract utils.sh
+read -rn 1 -p "Begin recording? [Y\n]" key
+case $key in
+y | Y | "") ;;
+*)
+  echo "aborting..."
+  exit 1
+  ;;
+esac
+
+systemd-inhibit --what "sleep:idle:shutdown" --who "gpu-screen-recorder" --why "recording screen" "${GPUSC_ARGS[@]}"
