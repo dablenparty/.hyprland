756414f961554cbcb827f759511c1e17dfa9f35a
dablenparty <anoptimisticcouch@gmail.com>
2025-06-26 19:12:49 -0400

    refactor(pkgbuild)!: separate out custom deps

---
+++ scripts/setup_on_arch.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+paru --rebuild=all --sudoloop -S blueman \
+  devtools \
+  firefox \
+  foot \
+  dot-hyprland/hypridle-git \
+  hyprlock-git \
+  hyprshot-git \
+  mako \
+  mpvpaper \
+  obsidian \
+  perl-image-exiftool \
+  python-pywal16 \
+  rofi-wayland \
+  sddm \
+  seatd \
+  socat \
+  speech-dispatcher \
+  swww-git \
+  tesseract \
+  tesseract-data-eng \
+  udiskie \
+  upscayl-ncnn \
+  waybar \
+  waypaper \
+  xdg-terminal-exec
