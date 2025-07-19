e7112c6e1f13c5ce7dbb99641e3e1b153bcafa9a
dablenparty <anoptimisticcouch@gmail.com>
2025-07-18 22:42:06 -0400

    refactor(monitors)!: monitorv2 syntax

--- scripts/rotate_monitor.sh
+++ scripts/rotate_monitor.sh
@@ -6,18 +6,4 @@ transform_num=${1:-0}
 
 # get the port ID for the currently focused monitor
 current_monitor="$(hyprctl monitors -j | jq --raw-output ".[] | select(.focused) | .name")"
-# WARN: this requires the monitor to be defined in the config
-# get existing rule for focused monitor from config
-# TODO: add handling for when the monitor is not in the config
-og_monitor_rule="$(rg --color=never -IN "^monitor=($current_monitor.+)$" --replace '$1' "$HOME/.config/hypr/monitors.conf")"
-
-if ((transform_num == 0)); then
-  # reset
-  monitor_rule="$og_monitor_rule"
-else
-  # rotate (transform) monitor
-  monitor_rule="${og_monitor_rule},transform,$transform_num"
-fi
-
-# actually execute transform
-hyprctl keyword monitor "$monitor_rule"
+notify-send -u critical "Failed to rotate $current_monitor" 'Monitor syntax has updated. Please fix the script!'
