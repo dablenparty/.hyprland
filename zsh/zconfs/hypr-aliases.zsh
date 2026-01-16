e7d9d0efa1a1873f925d0238ec395f6065aae8a0
dablenparty <anoptimisticcouch@gmail.com>
2026-01-15 19:52:17 -0500

    feat(zsh)!: hypr-aliases
    Aliases for my Hyprland setup

---
+++ zsh/zconfs/hypr-aliases.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+alias tvon='mon.zsh enable samsung-55-tv-2160@60.conf && mon.zsh primary samsung-55-tv-2160@60.conf'
+alias tvoff='mon.zsh disable samsung-55-tv-2160@60.conf && mon.zsh primary samsung-curved-1440@240.conf'
