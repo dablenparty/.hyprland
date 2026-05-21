6f2b74bd0dcc63c75aca40e99e0adc5347abdd71
dablenparty <anoptimisticcouch@gmail.com>
2026-05-21 19:27:17 -0400

    feat(pywal): output a Lua theme for hyprland

---
+++ pywal/templates/colors-hyprland.lua
@@ -0,0 +1,22 @@
+THEME = {}
+
+THEME.background = "rgb({background.strip})"
+THEME.foreground = "rgb({foreground.strip})"
+THEME.color0 = "rgb({color0.strip})"
+THEME.color1 = "rgb({color1.strip})"
+THEME.color2 = "rgb({color2.strip})"
+THEME.color3 = "rgb({color3.strip})"
+THEME.color4 = "rgb({color4.strip})"
+THEME.color5 = "rgb({color5.strip})"
+THEME.color6 = "rgb({color6.strip})"
+THEME.color7 = "rgb({color7.strip})"
+THEME.color8 = "rgb({color8.strip})"
+THEME.color9 = "rgb({color9.strip})"
+THEME.color10 = "rgb({color10.strip})"
+THEME.color11 = "rgb({color11.strip})"
+THEME.color12 = "rgb({color12.strip})"
+THEME.color13 = "rgb({color13.strip})"
+THEME.color14 = "rgb({color14.strip})"
+THEME.color15 = "rgb({color15.strip})"
+
+return THEME
