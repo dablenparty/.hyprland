a422a7f41371fe933d4ecd14119523150fc76eaf
dablenparty <anoptimisticcouch@gmail.com>
2026-07-20 18:28:44 -0400

    refactor(scripts)!: lua support

--- scripts/mon.zsh
+++ scripts/mon.zsh
@@ -39,78 +39,78 @@ cmd="$1"
 monitor="${2%.*}"
 
 case "$cmd" in
-  enable)
-    if check_enabled "$monitor"; then
-      printf '%s is already enabled!\n' "$monitor"
-      exit 0
-    fi
-    # enable
-    echo "require(\"monitors.$monitor\")" >> "$hyprconf_dir/monitors/active.lua"
-    if is_hyprland; then
-      hyprctl reload
-    fi
-    ;;
-  disable)
-    if ! check_enabled "$monitor"; then
-      printf "%s is already disabled!\n" "$monitor"
-      exit 0
-    fi
-    sed -Ei --follow-symlinks "/$monitor/d" "$hyprconf_dir/monitors/active.lua"
-    if is_hyprland; then
-      hyprctl reload
-    fi
-    ;;
-  primary)
-    if [[ -z "$monitor" ]]; then
-      # rg exits 1 if it fails to match
-      set +e
-      xrandr_monitor="${$(xrandr --listactivemonitors | rg --color=never -IN ' \d: \+\*([\w\d-]+).*' --replace '$1'):-none set}"
-      if [[ -r "$HOME/.primary_monitor" ]]; then
-        my_monitor="${$(<"$HOME/.primary_monitor"):-none set}"
-      else
-        my_monitor="none set"
-      fi
-      printf '  your primary monitor: %s\nxrandr primary monitor: %s\n' $my_monitor $xrandr_monitor
-      exit 0
-    fi
-
-    monitor_conf=${all_monitors["$monitor"]}
-    monitor_description="$(rg --color=never -IN '\s*output\s*=\s*"((desc:)?.+?)",$' --replace '$1' "$monitor_conf")"
-    if [[ $monitor_description =~ ^desc:(.+)$ ]]; then
-      output="$(hyprctl monitors -j | jq -r ".[] | select(.description == \"${match[1]}\") | .name")"
+enable)
+  if check_enabled "$monitor"; then
+    printf '%s is already enabled!\n' "$monitor"
+    exit 0
+  fi
+  # enable
+  echo "require(\"monitors.$monitor\")" >>"$hyprconf_dir/monitors/active.lua"
+  if is_hyprland; then
+    hyprctl reload
+  fi
+  ;;
+disable)
+  if ! check_enabled "$monitor"; then
+    printf "%s is already disabled!\n" "$monitor"
+    exit 0
+  fi
+  sed -Ei --follow-symlinks "/$monitor/d" "$hyprconf_dir/monitors/active.lua"
+  if is_hyprland; then
+    hyprctl reload
+  fi
+  ;;
+primary)
+  if [[ -z "$monitor" ]]; then
+    # rg exits 1 if it fails to match
+    set +e
+    xrandr_monitor="${$(xrandr --listactivemonitors | rg --color=never -IN ' \d: \+\*([\w\d-]+).*' --replace '$1'):-none set}"
+    if [[ -r "$HOME/.primary_monitor" ]]; then
+      my_monitor="${$(<"$HOME/.primary_monitor"):-none set}"
     else
-      output="$monitor_description"
+      my_monitor="none set"
     fi
-    printf 'setting primary to %s (%s)\n' "$monitor_description" "$output"
-    xrandr --output "$output" --primary
-    # no newline
-    printf '%s' "$output" > "$HOME/.primary_monitor"
-    ;;
-  reload)
-    mon.zsh disable "$monitor" && mon.zsh enable "$monitor"
-    if is_hyprland; then
-      hyprctl reload
-    fi
-    ;;
-  rotate)
-    monitor_conf=${all_monitors["$monitor"]}
-    transform_value=$3
-    # checks for uncommented line
-    if rg -q '^\s*transform' "$monitor_conf"; then
-      # already has transform line; modify it
-      sed_cmd="s/(\s*transform\s*=\s*)[0-9]/\1$transform_value/"
-    else
-      # doesn't have transform line; add it at the bottom
-      sed_cmd="/}/i \\    transform = $transform_value,"
-    fi
-    sed -Ei --follow-symlinks "$sed_cmd" "$monitor_conf"
+    printf '  your primary monitor: %s\nxrandr primary monitor: %s\n' $my_monitor $xrandr_monitor
+    exit 0
+  fi
 
-    if is_hyprland; then
-      hyprctl reload
-    fi
-    ;;
-  *)
-    printf "error: unknown command '%s'\n" "$cmd"
-    exit 2
-    ;;
+  monitor_conf=${all_monitors["$monitor"]}
+  monitor_description="$(rg --color=never -IN '\s*output\s*=\s*"((desc:)?.+?)",$' --replace '$1' "$monitor_conf")"
+  if [[ $monitor_description =~ ^desc:(.+)$ ]]; then
+    output="$(hyprctl monitors -j | jq -r ".[] | select(.description == \"${match[1]}\") | .name")"
+  else
+    output="$monitor_description"
+  fi
+  printf 'setting primary to %s (%s)\n' "$monitor_description" "$output"
+  xrandr --output "$output" --primary
+  # no newline
+  printf '%s' "$output" >"$HOME/.primary_monitor"
+  ;;
+reload)
+  mon.zsh disable "$monitor" && mon.zsh enable "$monitor"
+  if is_hyprland; then
+    hyprctl reload
+  fi
+  ;;
+rotate)
+  monitor_conf=${all_monitors["$monitor"]}
+  transform_value=$3
+  # checks for uncommented line
+  if rg -q '^\s*transform' "$monitor_conf"; then
+    # already has transform line; modify it
+    sed_cmd="s/(\s*transform\s*=\s*)[0-9]/\1$transform_value/"
+  else
+    # doesn't have transform line; add it at the bottom
+    sed_cmd="/}/i \\    transform = $transform_value,"
+  fi
+  sed -Ei --follow-symlinks "$sed_cmd" "$monitor_conf"
+
+  if is_hyprland; then
+    hyprctl reload
+  fi
+  ;;
+*)
+  printf "error: unknown command '%s'\n" "$cmd"
+  exit 2
+  ;;
 esac
--- scripts/rotate_monitor.zsh
+++ scripts/rotate_monitor.zsh
@@ -4,10 +4,10 @@ set -e
 setopt PIPE_FAIL
 
 hyprconf_dir="${XDG_CONFIG_HOME:-$HOME/.config}/hypr"
-active_monitor_dir="$hyprconf_dir/monitors/active"
+monitor_dir="$hyprconf_dir/monitors"
 transform_num=${1:-0}
 
 # get the currently focused monitor with jq and locate its config file with rg
 # this should only ever return one filename
-current_monitor_conf=${$(rg -l "output\s*=\s*desc:$(hyprctl monitors -j | jq --raw-output ".[] | select(.focused) | .description")" "$active_monitor_dir"/*.conf)##*/}
+current_monitor_conf=${$(rg -l "output\s*=\s*\"desc:$(hyprctl monitors -j | jq --raw-output ".[] | select(.focused) | .description")\"" "$monitor_dir"/*.lua)##*/}
 env mon.zsh rotate $current_monitor_conf $transform_num
